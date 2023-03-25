ad11e21c2992e03a287358eaa06be654eef2636c
faisalinfinity <mujtabafaisal944.fk@gmail.com>
2023-03-25 10:37:55 +0530

    changed folder structure

--- Routes/LoginRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { Login } = require("../Controller/Authcontroller");
-const LoginRoute = express.Router();
-
-LoginRoute.post("/", Login);
-
-module.exports = { LoginRoute };
--- Routes/RegisterRoute.js
+++
@@ -1,9 +0,0 @@
-const express=require("express")
-const { Register } = require("../Controller/Authcontroller")
-const Validator = require("../Middlewares/Validator")
-
-const RegisterRoute=express.Router()
-
-RegisterRoute.post("/",Validator, Register)
-
-module.exports={RegisterRoute}
